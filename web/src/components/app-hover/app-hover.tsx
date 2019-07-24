920115968a904fd4ccab3aae4af93ac76ae31bd7
Nicell <nick@winans.codes>
2019-07-24 14:05:33 -0500

    Fix hover element leaving page

--- web/src/components/app-hover/app-hover.tsx
+++ web/src/components/app-hover/app-hover.tsx
@@ -44,7 +44,7 @@ export class AppHover {
   render() {
     const w = this.state.data && this.state.data.w ? this.state.data.w : {};
     return this.state.visible ? (
-      <div class="app-hover" style={{left: this.state.x + 'px', top: this.state.y + 'px'}}>
+      <div class="app-hover" style={{ left: Math.min(Math.max(this.state.x, 272 / 2), document.documentElement.clientWidth - 272 / 2) + 'px', top: this.state.y + 'px', '--before-offset': `calc(50% + ${this.state.x - Math.min(Math.max(this.state.x, 272 / 2), document.documentElement.clientWidth - 272 / 2)}px)` }}>
         {this.state.data.c}, {this.state.data.s} {Object.keys(w).map(m => w[m].g - w[m].b).reduce((a,b) => a+b)}
         <div class="hover-charts">
           {Object.keys(w).map(m => (
